6b28269e899189d94228fa739febc7a17da8d15a
Nick Fedorov <nikola_fed1@mail.ru>
2023-08-31 01:43:10 +0300

    using 3rd-party module

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,11 @@
+const { v4: uuidv4 } = require('uuid');
+
+
 module.exports.handler = async function() {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello World!',
+            message: 'Hello World! ' + uuidv4(),
         }),
     }
 }
