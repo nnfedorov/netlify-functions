8d67f06f70abd6f7bff92f5d5b63df369371fd66
Nick Fedorov <nikola_fed1@mail.ru>
2023-09-01 01:13:39 +0300

    play with files

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,11 +1,28 @@
 const { v4: uuidv4 } = require('uuid');
+const { loadImage } = require('canvas');
+const fs = require('fs');
 
 
-module.exports.handler = async function() {
+module.exports.handler = async function(params, ctx) {
+    console.log(process.version);
+    
+    const { password } = params.queryStringParameters;
+
+    // if (password !== 'qqq') {
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify({ error: 'invalid password' })
+    //     };
+    // }
+    
+    const data = await fs.promises.readFile(__dirname + '/data.json', { encoding: 'UTF-8' });
+
+    console.log(JSON.stringify(data));
+    
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello World! ' + uuidv4(),
+            message: `Password = ${password}, operation id = ${uuidv4()}`
         }),
     }
 }
